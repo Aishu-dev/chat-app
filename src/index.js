54503788241d81e8db0f2d4fcc9049db2c1e7812
Aishwarya Narang <aishwarya.narang@cubastion.com>
2024-01-16 17:58:29 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+io.on('connection', (socket) => {
+    console.log('New Websocket connect!');
+
+    socket.emit('countupdated', count)
+
+});
+
+
+
+server.listen(port, () => {
+    console.log('Server is up and running on port', port);
+}); 
